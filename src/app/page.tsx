0d9c392a61daeefc154b64085e71abad3c1779eb
amadero170 <amadero170@gmail.com>
2023-08-04 14:27:02 -0600

    mejoras responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,26 @@ export default function Home() {
 
 return(<>
 <div
-className="bg-top bg-cover min-h-screen flex flex-col items-center px-4 lg:px-40 text-center"
+className="bg-top bg-cover min-h-screen flex flex-col items-center px-8 lg:px-40 text-center"
 style={{
   backgroundImage: `url("/bg-image.jpeg")`,
   backgroundColor:"#013230"
 }}>
-  <Image className="justify-center my-16 lg:my-32" src="/wos-logo-blanco.png" alt="wos" width={271} height={61}></Image>
-    <h3 className="font-black text-[44px] max-w-3xl my-4">Hola, somos WOS, una plataforma de estrategias digitales inmobiliarias.</h3>
-    <h4 className="text-2xl my-4">No queremos venderte nada, queremos ayudarte a<br></br> <i className=" font-black">trabajar con más poder:</i></h4>
-  <div className="grid grid-cols-2 gap-4">
-  <ul className="text-left text-xl mt-4 mx-2 lg:mx-8 font-medium">
-    <li className="my-8">→ Mejores leads digitales</li>
-    <li className="my-8">→ Más opciones de venta</li>
-    <li className="my-8">→ Mejores comisiones</li>
-  </ul>
-  <ul className="text-left text-xl mt-4 mx-2 lg:mx-8 font-medium">
-    <li className="my-8">→ Estrategias reales de marketing</li>
-    <li className="my-8">→ Capacitación innovadora</li>
-    <li className="my-8">→ Motivación</li> 
-  </ul>
-</div>
+  <Image className="justify-center my-20 lg:my-32" src="/wos-logo-blanco.png" alt="wos" width={271} height={61}></Image>
+    <h3 className="font-black text-[40px] lg:text-[44px] max-w-3xl my-4">Hola, somos WOS, una plataforma de estrategias digitales inmobiliarias.</h3>
+    <h4 className="text-xl lg:text-2xl my-4">No queremos venderte nada, queremos ayudarte a<br></br> <i className=" font-black">trabajar con más poder:</i></h4>
+    <ul className="grid grid-cols-2 grid-rows-3 gap-2 lg:gap-4 grid-flow-row grid-auto-rows-auto text-left text-lg md:text-xl mt-4 mx-2 lg:mx-8 font-medium">
+        <li className="my-2 md:my-8">→ Mejores leads digitales</li>
+        <li className="my-2 md:my-8">→ Más opciones de venta</li>
+        <li className="my-2 md:my-8">→ Mejores comisiones</li>
+        <li className="my-2 md:my-8">→ Estrategias reales de marketing</li>
+        <li className="my-2 md:my-8">→ Capacitación innovadora</li>
+        <li className="my-2 md:my-8">→ Motivación</li> 
+
+</ul>
   <Image className="justify-center my-12" src="/flecha.png" alt="wos" width={50} height={50}></Image>
   <div>
-  <h3 className="font-black text-4xl my-16">WOS está listo para iniciar operación en Guadalajara, Puerto Vallarta & Riviera Nayarit </h3>
+  <h3 className="font-black text-[40px] lg:text-[44px] my-8 lg:my-16">WOS está listo para iniciar operación en Guadalajara, Puerto Vallarta & Riviera Nayarit </h3>
   <h4 className="text-2xl mb-2">Estamos formando un pequeño grupo de profesionales inmobiliarios con visión que quieran ser parte del equipo fundador.</h4>
   <h4 className="text-2xl md:text-3xl lg:text-4xl font-black text-[#70EAED]">#AsesoresInnovadores</h4>
   </div>
